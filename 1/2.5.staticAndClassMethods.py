fbeb79c033ff3f769c5b49ee7b6d697c3599d406
Michal Mierzejewski <micham35@gmail.com>
2022-11-12 13:44:54 +0100

    Finished that lab

--- 1/2.5.staticAndClassMethods.py
+++ 1/2.5.staticAndClassMethods.py
@@ -1,5 +1,6 @@
 class Watch:
     __watches_created = 0
+
     def __init__(self):
         Watch.__watches_created += 1
 
@@ -8,13 +9,28 @@ class Watch:
         _watch = cls()
         _watch.engraving = text
         return _watch
+
     @classmethod
     def get_number_of_watches_created(cls):
         return cls.__watches_created
 
     @staticmethod
     def validate(text):
-        if len(text) < 40:
-            pass
-        else:
-            return False
+        if len(text) < 40 and text.isalnum():
+            return True
+        return False
+        # raise Exception
+
+
+print(Watch.get_number_of_watches_created())
+watch = Watch()
+print(Watch.get_number_of_watches_created())
+try:
+    if Watch.validate('siema123'):
+        watcheng = Watch.dedicated_watch('siema123')
+    print(Watch.get_number_of_watches_created())
+    if Watch.validate('d@pa'):
+        watcheng2 = Watch.dedicated_watch('d@pa')
+    print(Watch.get_number_of_watches_created())
+except Exception as e:
+    print("Engraving is not so good", e)
