c04df74bc930634ca8703a31a886bfa86c4a9804
Michal <micham35@gmail.com>
2022-11-14 14:34:13 +0100

    finished lab 2.6

--- 1/2.6abstractClasses.py
+++ 1/2.6abstractClasses.py
@@ -28,16 +28,25 @@ class MFD1(Scanner, Printer):
         print('Scanned doc')
 
     def get_scanner_status(self):
-        print(f'Max res is {MFD1.res}x{MFD1.res}')
+        print(f'Max res is {self.res}x{self.res}')
 
     def print_document(self):
         print('Printed doc')
 
     def get_printer_status(self):
-        print(f'Max res is {MFD1.res}x{MFD1.res}')
+        print(f'Max res is {self.res}x{self.res}')
 
 class MFD2(MFD1):
-    res = 500
+    def __init__(self):
+        self.res = 500
+
+
+class MFD3(MFD1):
+    def __init__(self):
+        self.res = 1000
+
+    def fax(self):
+        print('fax sent')
 
 
 mfd1 = MFD1()
@@ -47,4 +56,9 @@ mfd1.get_printer_status()
 mfd2 = MFD2()
 mfd2.get_scanner_status()
 mfd2.get_printer_status()
+
+mfd3 = MFD3()
+mfd3.get_scanner_status()
+mfd3.get_printer_status()
+mfd2.fax()
 
