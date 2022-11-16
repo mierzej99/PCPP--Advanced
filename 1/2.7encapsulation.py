0164a77e9f1d16c05f3d0b5a48f74272eecb8cbe
Michal Mierzejewski <micham35@gmail.com>
2022-11-16 12:12:27 +0100

    finished lab 2.7, encapsulation

--- 1/2.7encapsulation.py
+++ 1/2.7encapsulation.py
@@ -5,20 +5,55 @@ class AccountException(Exception):
 class BankAccount():
     latestAccountNumber = 0
 
-    def __init__(self):
+    def __init__(self, balance):
         self.__accountNumber = BankAccount.latestAccountNumber
-        self.accountBalance = 0
+        self.__accountBalance = balance
         BankAccount.latestAccountNumber += 1
+
+    @property
+    def accountBalance(self):
+        return self.__accountBalance
+
+    @accountBalance.setter
+    def accountBalance(self, amount):
+        if amount < 0:
+            raise AccountException('Balance cant be negative')
+        else:
+            if abs(amount-self.__accountNumber) > 100000:
+                print("Very big operation of ", amount-self.__accountBalance)
+            self.__accountBalance = amount
 
     @property
     def accountNumber(self):
         return self.__accountNumber
 
     @accountNumber.setter
-    def accountNumber(self):
-        raise AccountException
+    def accountNumber(self, number):
+        raise AccountException('You caant change account number')
 
     @accountNumber.deleter
     def accountNumber(self):
-        if self.balance != 0:
-            raise AccountException
+        if self.accountBalance != 0:
+            raise AccountException('You cant delete account with positive balance')
+        else:
+            del self
+
+
+firstAccount = BankAccount(1000)
+try:
+    firstAccount.accountBalance -= 1200
+except AccountException as E:
+    print(E)
+try:
+    firstAccount.accountNumber = 17
+except AccountException as E:
+    print(E)
+try:
+    firstAccount.accountBalance += 1000000
+except AccountException as E:
+    print(E)
+try:
+    del firstAccount.accountNumber
+except AccountException as E:
+    print(E)
+
