7e96aca9cb2065c85c9334baa1d3d38e2c63fdf8
Michal Mierzejewski <micham35@gmail.com>
2022-12-31 12:27:49 +0100

    lab server availability checker using requests library

--- 4/requestsServerChecker.py
+++ 4/requestsServerChecker.py
@@ -2,15 +2,22 @@ import requests
 import sys
 
 if len(sys.argv) not in [2, 3]:
-    exit('Improper number of arguments',1)
+    exit('Improper number of arguments', 1)
 if sys.argv[2]:
     try:
-        port_number = int(sys.argv[2])
+        port_number = sys.argv[2]
     except ValueError:
         exit(2)
 else:
-    port_number = 80
+    port_number = '80'
 
+url = sys.argv[1] + f":{port_number}" if sys.argv[:4] == 'http' else 'http://' + sys.argv[1] + f":{port_number}"
 
 try:
-    reply = requests.head()
+    reply = requests.head(url=url)
+except requests.exceptions.ConnectTimeout:
+    exit(3)
+except requests.exceptions.RequestException:
+    exit(4)
+
+print(reply)
