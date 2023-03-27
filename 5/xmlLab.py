055d57afd7c15e97f65853e27f55c1ef3d49e15f
Michal <micham35@gmail.com>
2023-03-27 15:19:22 +0200

    finished xml

--- 5/xmlLab.py
+++ 5/xmlLab.py
@@ -1,12 +1,20 @@
-import xml
+import xml.etree.ElementTree as ET
 
 
 class TemperatureConverter:
-    def convert_celcius_to_fahrenheit(self, celcius):
+    @staticmethod
+    def convert_celcius_to_fahrenheit(celcius):
         return 9/5 * celcius + 32
 
 class ForecastXmlParser:
 
     def parse(self):
-        tree = xml.etree.ElementTree.parse('forecast.xml')
-        ET = tree.getroot()
+        tree = ET.parse('forecast.xml')
+        root = tree.getroot()
+        for item in root:
+            print(f'{item[0].text}: {int(item[1].text)} Celcius, {TemperatureConverter.convert_celcius_to_fahrenheit(int(item[1].text))} Fahrenheit')
+
+
+forcast = ForecastXmlParser()
+
+forcast.parse()
